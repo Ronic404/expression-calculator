3c4285e7d5f5948d7bee535b386e49d3515f9b02
ROnic404 <ronic@inbox.ru>
2020-03-07 17:59:53 +0400

    feat: add solution easy level

--- src/index.js
+++ src/index.js
@@ -4,7 +4,56 @@ function eval() {
 }
 
 function expressionCalculator(expr) {
-    // write your solution here
+    let charsArray = [];
+    if (/\s/.test(expr)) {
+        charsArray = expr.trim().split(/\s/)
+    } else {
+        charsArray = expr.split("");
+    }    
+
+    for(let i = 0; i < charsArray.length; i++) {
+        if(charsArray[i] === "*") {
+            charsArray[i-1] = +charsArray[i-1] * +charsArray[i+1];
+            charsArray.splice(i,2);
+            i = 0;
+        }
+        if(charsArray[i] === "/") {
+            if(charsArray[i+1] != 0) {
+                charsArray[i-1] = +charsArray[i-1] / +charsArray[i+1];
+                charsArray.splice(i,2);
+                i = 0;
+            } else {
+                throw new Error("TypeError: Division by zero.");
+            }
+        }        
+    }
+
+    for(let i = 0; i < charsArray.length; i++) {
+        if(charsArray[i] === "+") {
+            charsArray[i-1] = +charsArray[i-1] + +charsArray[i+1];
+            charsArray.splice(i,2);
+            i = 0;
+        }
+        if(charsArray[i] === "-") {
+            charsArray[i-1] = +charsArray[i-1] - +charsArray[i+1];
+            charsArray.splice(i,2);
+            i = 0;
+        }
+    }   
+
+    
+    let openBrackets = 0;
+    let closeBrackets = 0;
+    for(let i of expr) {
+        if(i === "(") openBrackets++;
+        if(i === ")") closeBrackets++;
+        if(openBrackets !== closeBrackets) {
+            throw new Error("ExpressionError: Brackets must be paired")
+        }
+    }
+
+    //console.log(expr);
+    return charsArray[0];
 }
 
 module.exports = {
